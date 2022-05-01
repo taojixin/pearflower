b46991bd1b9b899b17120d51cd56137e8a12a861
Tao <491675919@qq.com>
2022-05-01 22:15:56 +0800

    2022.5.1

--- src/views/personal/Account.vue
+++ src/views/personal/Account.vue
@@ -1,20 +1,30 @@
 <template>
   <div class="account">
     <div class="character">
-      <img src="" alt="">
+      <img src="" alt="" />
       <span class="one">您好！XXX</span>
-      <span class="two">账户余额：<b>￥0.00</b>&nbsp;&nbsp;&nbsp;&nbsp;会员级别：<b>白金会员</b></span>
-      <span class="three">我的积分：<b>0</b> 分&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;优&nbsp; 惠&nbsp; 价：<b>0</b> 张</span>
+      <span class="two"
+        >账户余额：<b>￥0.00</b>&nbsp;&nbsp;&nbsp;&nbsp;会员级别：<b
+          >白金会员</b
+        ></span
+      >
+      <span class="three"
+        >我的积分：<b>0</b>
+        分&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;优&nbsp; 惠&nbsp;
+        价：<b>0</b> 张</span
+      >
       <div class="left">带处理订单(0)</div>
       <div class="center">待评价订单(0)</div>
       <div class="await">待支付订单(0)</div>
     </div>
-    <hr>
+    <hr />
     <el-tabs type="card">
-    <el-tab-pane label="最近浏览记录" value="first">暂无最近浏览记录</el-tab-pane>
-    <el-tab-pane label="购买记录" name="second">购买记录</el-tab-pane>
-    <el-tab-pane label="收藏夹" name="third">收藏夹</el-tab-pane>
-    <el-tab-pane label="本周热销" name="fourth">本周热销</el-tab-pane>
+      <el-tab-pane label="最近浏览记录" value="first"
+        >暂无最近浏览记录</el-tab-pane
+      >
+      <el-tab-pane label="购买记录" name="second">暂无购买记录</el-tab-pane>
+      <el-tab-pane label="收藏夹" name="third">收藏夹为空</el-tab-pane>
+      <el-tab-pane label="本周热销" name="fourth">暂无相关信息</el-tab-pane>
     </el-tabs>
   </div>
 </template>
@@ -25,11 +35,11 @@ export default {};
 
 <style lang="less">
 .el-tabs__item.is-active {
-    color: green!important;
+  color: green !important;
 }
 .el-tabs__item:hover {
-    color: green!important;
-    cursor: pointer;
+  color: green !important;
+  cursor: pointer;
 }
 .character {
   height: 200px;
@@ -37,7 +47,13 @@ export default {};
   // border: 1px solid black;
   position: relative;
 
-  img,.one,.two,.three,.left,.center,.await {
+  img,
+  .one,
+  .two,
+  .three,
+  .left,
+  .center,
+  .await {
     position: absolute;
   }
 
@@ -60,7 +76,9 @@ export default {};
     top: 100px;
     left: 200px;
   }
-  .left, .center, .await {
+  .left,
+  .center,
+  .await {
     width: 80px;
     height: 30px;
     line-height: 30px;
@@ -79,7 +97,5 @@ export default {};
   .await {
     left: 400px;
   }
-
-  
 }
 </style>
--- src/views/personal/Paypassword.vue
+++ src/views/personal/Paypassword.vue
@@ -1,15 +1,98 @@
 <template>
   <div class="paypassword">
-    支付密码
+    <h2>支付密码</h2>
+    <el-tabs type="card">
+      <el-tab-pane label="设置支付密码" value="first">
+        <div class="password setpassword">
+          <div class="text">
+            <span>* </span>输入验证码：
+            <input type="text" />
+          </div>
+          <div class="text">
+            <span>* </span>输入新密码：
+            <input type="text" />
+          </div>
+          <div class="text">
+            <span>* </span>确认新密码：
+            <input type="text" />
+          </div>
+          <button>保存</button>
+        </div>
+      </el-tab-pane>
+      <el-tab-pane label="修改支付密码" name="second">
+        <div class="password changepassword">
+          <div class="text">
+            <span>* </span>输入旧密码：
+            <input type="text" />
+          </div>
+          <div class="text">
+            <span>* </span>输入新密码：
+            <input type="text" />
+          </div>
+          <div class="text">
+            <span>* </span>确认新密码：
+            <input type="text" />
+          </div>
+          <button>保存</button>
+        </div>
+      </el-tab-pane>
+      <el-tab-pane label="找回支付密码" name="third">
+        <div class="password changepassword">
+          <div class="text">
+            <span>* </span>输入手机号：
+            <input type="text" />
+          </div>
+          <div class="text">
+            <span>* </span>输入验证码：
+            <input type="text" />
+          </div>
+          <div class="text">
+            <span>* </span>输入新密码：
+            <input type="text" />
+          </div>
+          <div class="text">
+            <span>* </span>确认新密码：
+            <input type="text" />
+          </div>
+          <button class="last">保存</button>
+        </div>
+      </el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
+<style lang="less">
+.password {
+  width: 100%;
+  height: 100vh;
+  position: relative;
+  margin: 20px;
+  color: green;
+  .text {
+    margin-bottom: 20px;
+    span {
+      color: red;
+      text-align: center;
+      vertical-align: middle; // 让*居中
+    }
+  }
 
+  button {
+    position: absolute;
+    top: 150px;
+    left: 120px;
+    width: 80px;
+    height: 40px;
+    font-size: 16px;
+    background-color: rgb(179, 244, 179);
+    border: none;
+  }
+  .last {
+    top: 170px;
+  }
+}
 </style>
